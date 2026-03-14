2063fbbf86af91fbdba0a5e5a4157b7876ebc366
Santiago Giraldo <santiago.giraldo-c@mail.escuelaing.edu.co>
2026-03-14 10:31:14 -0500

    Implement Robot class with movement and direction

---
+++ robot-simulator.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <stdexcept>
+using namespace std;
+
+enum Direction { NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3 };
+
+class Robot {
+private:
+    int x, y;
+    Direction dir;
+
+    const int dx[4] = {0, 1, 0, -1};
+    const int dy[4] = {1, 0, -1, 0};
+
+public:
+    Robot(int x, int y, Direction dir) : x(x), y(y), dir(dir) {}
+
+    int getX()         const { return x; }
+    int getY()         const { return y; }
+    Direction facing() const { return dir; }
+
+    void turnRight() {
+        dir = static_cast<Direction>((dir + 1) % 4);
+    }
+
+    void turnLeft() {
+        dir = static_cast<Direction>((dir + 3) % 4);
+    }
+
+    void advance() {
+        x += dx[dir];
+        y += dy[dir];
+    }
+
+    void execute(const string& instructions) {
+        for (char cmd : instructions) {
+            if      (cmd == 'R') turnRight();
+            else if (cmd == 'L') turnLeft();
+            else if (cmd == 'A') advance();
+            else throw invalid_argument("Instruccion invalida: " + string(1, cmd));
+        }
+    }
+};
